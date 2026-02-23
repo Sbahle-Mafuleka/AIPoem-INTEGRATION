decf96d1516e9441af614f37761b92ec0f1ae014
Sbahle-Mafuleka <sbahlemagubs@gmail.com>
2026-02-23 07:22:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function generator(response) {
+  let poemText = document.querySelector("#poem-text");
+  poemText.innerHTML = "";
+
+  new Typewriter(poemText, {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 50,
+  });
+}
+
+let apiKey = "d03cbc9of2636a047c1f18afat64d481";
+let context = "let it be a short poem";
+
+function getPoem(event) {
+  event.preventDefault();
+
+  let poemText = document.querySelector("#poem-text");
+  let button = document.querySelector(".sub");
+  let userInput = document.querySelector("#prompter").value;
+
+  poemText.innerHTML = "⏳ Generating your poem...";
+  button.disabled = true;
+
+  let prompt = `Give me a poem with the topic ${userInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(function (response) {
+    generator(response);
+    button.disabled = false;
+  });
+}
+
+let poemElement = document.querySelector("#vernac");
+poemElement.addEventListener("submit", getPoem);
